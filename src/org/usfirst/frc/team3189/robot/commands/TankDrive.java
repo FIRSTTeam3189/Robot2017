86faf9aad3769b64691ccc1bd439154916973a77
Alex Rodgers <alex.e.rodgers@gmail.com>
2017-01-11 16:56:22 -0800

    Fix TODO's in TankDrive command

--- src/org/usfirst/frc/team3189/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team3189/robot/commands/TankDrive.java
@@ -5,18 +5,27 @@ import org.usfirst.frc.team3189.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- *
+ * Drives the robot
+ *@author Deez Nuts and Alex and Harambe
  */
 public class TankDrive extends Command {// TODO fill in java doc
-
+/**
+ * 
+ */
 	public TankDrive() {
+		/**
+		 * Uses the Robots drivetrain's subsystem
+		 */
 		requires(Robot.drivetrain);
 	}
 
 	protected void initialize() {
 	}
-
+	
 	protected void execute() {
+		/**
+		 * Get speed from Joysticks and Drives bot
+		 */
 		Robot.drivetrain.tankDrive(Robot.oi.getLeftY(), Robot.oi.getRightY());
 	}
 
@@ -24,11 +33,18 @@ public class TankDrive extends Command {// TODO fill in java doc
 		return false;
 	}
 
-	protected void end() {
+	protected void end(){
+		/**
+		 * Stops the robot when their is no input from the Joysticks
+		 */
+		
 		Robot.drivetrain.tankDrive(0, 0);
 	}
 	
 	protected void interrupted() {
+		/**
+		 * Stops the robot when another Command is called
+		 */
 		Robot.drivetrain.tankDrive(0, 0);
 	}
 }
