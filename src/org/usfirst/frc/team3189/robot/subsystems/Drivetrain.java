1b89de39f321377997fa8680bc2072c856168158
Alex Rodgers <alex.e.rodgers@gmail.com>
2017-01-11 16:42:54 -0800

    Fix all TODO's in Drivetrain.

--- src/org/usfirst/frc/team3189/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team3189/robot/subsystems/Drivetrain.java
@@ -1,34 +1,56 @@
 package org.usfirst.frc.team3189.robot.subsystems;
 
+import org.usfirst.frc.team3189.robot.commands.TankDrive;
+
 import com.ctre.CANTalon;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *
+ * This provides a interface for the drivetrain command for the 2017 bot
+ * 
+ * @author Nate and Nick
+ * 
  */
 public class Drivetrain extends Subsystem {
 
-	private CANTalon leftFront = new CANTalon(0);
-	private CANTalon leftMiddle = new CANTalon(0);//TODO map port numbers in robotmap.
+	private CANTalon leftFront = new CANTalon(2);
+	private CANTalon leftMiddle = new CANTalon(1);
 	private CANTalon leftBack = new CANTalon(0);
-	private CANTalon rightFront = new CANTalon(0);
-	private CANTalon rightMiddle = new CANTalon(0);
-	private CANTalon rightBack = new CANTalon(0);
+	private CANTalon rightFront = new CANTalon(5);
+	private CANTalon rightMiddle = new CANTalon(4);
+	private CANTalon rightBack = new CANTalon(3);
 	
-	public void tankDrive(double left, double right){//TODO add java docs for everything in here including the class.
+	public Drivetrain() {
+		/**
+		 * This inverts the right motors on the robot.
+		 */
+		rightFront.setInverted(true);
+		rightMiddle.setInverted(true);
+		rightBack.setInverted(true);
+		
+	}
+	
+	public void tankDrive(double left, double right){
+		/**
+		 * Sets the speeds of the left and right motors for the tankdrve
+		 */
 		leftFront.set(left);
 		leftBack.set(left);
 		leftMiddle.set(left);
-		rightFront.set(right);//TODO right motors need to be oposite of left. use isInverted in contructor. 
+		rightFront.set(right);
 		rightMiddle.set(right);
 		rightBack.set(right);
 	}
 
     public void initDefaultCommand() {
+    	/**
+    	 * Sets the default command to Tankdrive
+    	 */
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-    	//TODO set default command for the drivetrain
+    	
+    	setDefaultCommand(new TankDrive());
     }
 }
 
