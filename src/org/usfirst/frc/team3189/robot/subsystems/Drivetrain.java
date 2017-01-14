6746a5e64eeb182626860d49e28f584031b5e45f
Alex Rodgers <alex.e.rodgers@gmail.com>
2017-01-13 19:17:30 -0800

    Mapped the motor controllers to the RobotMap and added the variables to Drvietrain class.

--- src/org/usfirst/frc/team3189/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team3189/robot/subsystems/Drivetrain.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team3189.robot.subsystems;
 
+import org.usfirst.frc.team3189.robot.Robot;
+import org.usfirst.frc.team3189.robot.RobotMap;
 import org.usfirst.frc.team3189.robot.commands.TankDrive;
 
 import com.ctre.CANTalon;
@@ -14,17 +16,18 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * 
  */
 public class Drivetrain extends Subsystem {
-
-	private CANTalon leftFront = new CANTalon(2);
+	
 	/**
 	 * The {@link SpeedController} for the left front motor of the
 	 * {@link Drivetrain}
 	 */
-	private CANTalon leftMiddle = new CANTalon(1);
-	private CANTalon leftBack = new CANTalon(0);
-	private CANTalon rightFront = new CANTalon(5);
-	private CANTalon rightMiddle = new CANTalon(4);
-	private CANTalon rightBack = new CANTalon(3);
+	private CANTalon leftFront = new CANTalon(RobotMap.leftFront);
+	
+	private CANTalon leftMiddle = new CANTalon(RobotMap.leftMiddle);
+	private CANTalon leftBack = new CANTalon(RobotMap.leftBack);
+	private CANTalon rightFront = new CANTalon(RobotMap.rightFront);
+	private CANTalon rightMiddle = new CANTalon(RobotMap.rightMiddle);
+	private CANTalon rightBack = new CANTalon(RobotMap.rightBack);
 
 	public Drivetrain() {
 		/**
