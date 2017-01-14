2a8fcc858f7ae8d39ca670bea2afc4215a5848f7
Yup Thats Me <Yup Thats Me@10.6.176.103>
2017-01-13 18:28:37 -0800

    Fixed Documentations

--- src/org/usfirst/frc/team3189/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team3189/robot/subsystems/Drivetrain.java
@@ -14,31 +14,31 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * 
  */
 public class Drivetrain extends Subsystem {
-
-	private CANTalon leftFront = new CANTalon(2);
 	/**
 	 * The {@link SpeedController} for the left front motor of the {@link Drivetrain}
 	 */
+	private CANTalon leftFront = new CANTalon(2);
+	
 	private CANTalon leftMiddle = new CANTalon(1);
 	private CANTalon leftBack = new CANTalon(0);
 	private CANTalon rightFront = new CANTalon(5);
 	private CANTalon rightMiddle = new CANTalon(4);
 	private CANTalon rightBack = new CANTalon(3);
-	
+	/**
+	 * This inverts the right motors on the robot.
+	 */
 	public Drivetrain() {
-		/**
-		 * This inverts the right motors on the robot.
-		 */
+		
 		rightFront.setInverted(true);
 		rightMiddle.setInverted(true);
 		rightBack.setInverted(true);
 		
 	}
-	
+	/**
+	 * Sets the speeds of the left and right motors for the tankdrve
+	 */
 	public void tankDrive(double left, double right){
-		/**
-		 * Sets the speeds of the left and right motors for the tankdrve
-		 */
+		
 		leftFront.set(left);
 		leftBack.set(left);
 		leftMiddle.set(left);
@@ -46,11 +46,11 @@ public class Drivetrain extends Subsystem {
 		rightMiddle.set(right);
 		rightBack.set(right);
 	}
-
+	/**
+	 * Sets the default command to Tankdrive
+	 */
     public void initDefaultCommand() {
-    	/**
-    	 * Sets the default command to Tankdrive
-    	 */
+    	
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     	
