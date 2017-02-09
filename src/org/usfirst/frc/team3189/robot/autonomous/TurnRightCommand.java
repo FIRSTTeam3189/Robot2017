a5caed7b3ce332759109a014538a558ff81d4ec6
Redsnug <redsnug1@gmail.com>
2017-02-08 17:52:36 -0800

    I program left movement

--- src/org/usfirst/frc/team3189/robot/autonomous/TurnRightCommand.java
+++ src/org/usfirst/frc/team3189/robot/autonomous/TurnRightCommand.java
@@ -1,44 +1,28 @@
 package org.usfirst.frc.team3189.robot.autonomous;
 
-import org.usfirst.frc.team3189.robot.Constants;
-import org.usfirst.frc.team3189.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class TurnLeftCommand extends Command {
+public class TurnRightCommand extends Command {
 
-	public double time;
-	
-    public TurnLeftCommand(double time) {
-    	
-    	requires(Robot.drivetrain);
-    	
-    	this.time = time;
-    	
-    	
+    public TurnRightCommand() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
-    	setTimeout(time);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
-    	Robot.drivetrain.tankDrive(-Constants.AUTO_FORWARD_SPEED, Constants.AUTO_FORWARD_SPEED);
-    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return isTimedOut();
+        return false;
     }
 
     // Called once after isFinished returns true
@@ -48,7 +32,5 @@ public class TurnLeftCommand extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-
-    	Robot.drivetrain.tankDrive(0, 0);
     }
 }
